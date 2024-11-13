f943d5c32dda5716ca689ce74eec3c21810075cb
AGalvan-College <andrea.galvan100@student.tsc.edu>
2024-11-13 02:07:14 -0600

    Fix Nav

--- HTML-Template/jQuery/js/main.js
+++ HTML-Template/jQuery/js/main.js
@@ -1,19 +1,28 @@
-$(document).ready(function() {
-    
-    $('#mobilenav_id').load('mobile_headers.html');
-    $('#footer_id').load('footers.html');
 
-    $('.hamburger-menu').click(function(){
-        event.stopPropagation();
-        
-        if ($('main').hasClass('active')) {
-            $('main').removeClass('active');
-        } else {
-            $('main').addClass('active');
-        }
+$(function(){
+      $("#header_id").load("header.html");
+        $('#mobilenav_id').load('mobile_header.html');
+        $('#footer_id').load('footers.html');
     });
+
+$(document).ready(function() {
+	// load header html
+	$( "#header_id" ).load( "header.html", function() {
+		console.log( "external header loaded" );
+	});
+
+	// includes/footer-nav.html
+	$( "#footer_id" ).load( "footers.html", function() {
+		console.log( "external footer loaded" );
+	});
+
+    // includes/footer-nav.html
+	$( "#mobilenav_id" ).load( "mobile_header.html", function() {
+		console.log( "external mobile header loaded" );
+	});
     
-    $('main').click(function(){
-        $('main').removeClass('active');
-    });
+	// load external styles
+	$("head").append('<script src="js/hamburger.js"></script>');
+
+//END DOC READY
 });
